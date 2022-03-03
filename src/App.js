4f795b675d306eceddebc7a1b8e0b90e2756fbd1
Marcos Andre <marcosdev.me@gmail.com>
2022-03-03 11:37:07 -0300

    feat: create featured movie component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import "./App.css";
 import Api from "./services/api";
 
 import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
 
 const App = () => {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       const list = await Api.getHomeList();
-      console.log(list);
       setMovieList(list);
+
+      const originals = list.filter((i) => i.slug === "originals");
+      const randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      const chosen = originals[0].items.results[randomChosen];
     };
 
     loadAll();
@@ -23,6 +30,7 @@ const App = () => {
       {/* Header */}
 
       {/* Destaque */}
+      {featuredData && <FeaturedMovie item={featuredData} />}
 
       {/* Listas */}
       <section className="lists">
---
+++ src/components/FeaturedMovie/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const FeaturedMovie = () => {
+  return (
+    <>
+      <h1>Featured</h1>
+    </>
+  );
+};
+
+export default FeaturedMovie;
