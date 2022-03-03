b4f55e916b6c31c899732a2b076cb8622239d64d
Marcos Andre <marcosdev.me@gmail.com>
2022-03-03 12:27:45 -0300

    feat: add featured movie

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./style.css";
 
 const FeaturedMovie = ({ item }) => {
+  const firstDate = new Date(item.first_air_date);
+
+  const genres = [];
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
+
   return (
     <section
       className="featured"
@@ -13,7 +20,29 @@ const FeaturedMovie = ({ item }) => {
     >
       <div className="featured--vertical">
         <div className="featured--horizontal">
-          <div className="">{item.original_name}</div>
+          <div className="featured--name">{item.original_name}</div>
+          <div className="featured--info">
+            <div className="featured--points">{item.vote_average} pontos</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
+            <div className="featured--seasons">
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons !== 1 ? "s" : ""}
+            </div>
+          </div>
+          <div className="featured--description">{item.overview}</div>
+
+          <div className="featured--buttons">
+            <a href={`/watch/${item.id}`} className="featured--watchButton">
+              ► Assistir
+            </a>
+            <a href={`/list/add/${item.id}`} className="featured--mylistbutton">
+              + Minha Lista
+            </a>
+          </div>
+
+          <div className="featured--genres">
+            <strong>Gêneros: </strong> {genres.join(", ")}
+          </div>
         </div>
       </div>
     </section>
