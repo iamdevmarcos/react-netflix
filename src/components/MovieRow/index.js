75820a2d3d9db1e8a0ade28725568e0c820d598a
Marcos Andre <marcosdev.me@gmail.com>
2022-03-03 15:21:14 -0300

    style: add arrows

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./style.css";
 
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+
 const MovieRow = ({ title, items }) => {
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+
+      <div className="movieRow--left">
+        <NavigateBeforeIcon style={{ fontSize: 50 }} />
+      </div>
+
+      <div className="movieRow--right">
+        <NavigateNextIcon style={{ fontSize: 50 }} />
+      </div>
+
       <div className="movieRow--listarea">
         <div className="movieRow--list">
           {items.results.length > 0 &&
