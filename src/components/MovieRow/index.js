f9a0fecbacd1b27ce6f1e096659e28560ae25e0c
Marcos Andre <marcosdev.me@gmail.com>
2022-03-03 11:21:40 -0300

    style: add styles to movie row

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -3,16 +3,20 @@ import "./style.css";
 
 const MovieRow = ({ title, items }) => {
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
       <div className="movieRow--listarea">
-        {items.results.length > 0 &&
-          items.results.map((item, index) => (
-            <img
-              src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-              alt={item.original_title}
-            />
-          ))}
+        <div className="movieRow--list">
+          {items.results.length > 0 &&
+            items.results.map((item, index) => (
+              <div className="movieRow--item" key={index}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
