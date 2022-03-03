ddffa996c33dd2a803c815c189eb7a6a600946f5
Marcos Andre <marcosdev.me@gmail.com>
2022-03-03 16:07:32 -0300

    style: add responsive

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -5,7 +5,7 @@ import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 
 const MovieRow = ({ title, items }) => {
-  const [scrollX, setScrollX] = useState(-400);
+  const [scrollX, setScrollX] = useState(0);
 
   const handleLeftArrow = () => {
     let x = scrollX + Math.round(window.innerWidth / 2);
