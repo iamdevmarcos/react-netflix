5913369f2b787c6567bc8bb620f9332171d2f582
Marcos Andre <marcosdev.me@gmail.com>
2022-03-03 15:05:59 -0300

    perf: add substring function

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -9,6 +9,11 @@ const FeaturedMovie = ({ item }) => {
     genres.push(item.genres[i].name);
   }
 
+  let description = item.overview;
+  if (description.length > 200) {
+    description = description.substring(0, 200) + "...";
+  }
+
   return (
     <section
       className="featured"
@@ -29,7 +34,7 @@ const FeaturedMovie = ({ item }) => {
               {item.number_of_seasons !== 1 ? "s" : ""}
             </div>
           </div>
-          <div className="featured--description">{item.overview}</div>
+          <div className="featured--description">{description}</div>
 
           <div className="featured--buttons">
             <a href={`/watch/${item.id}`} className="featured--watchButton">
