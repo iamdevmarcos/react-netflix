3ae26896276882b0384dc0b0a94614905246faa1
Marcos Andre <marcosdev.me@gmail.com>
2022-03-04 09:26:09 -0300

    feat: add movie player

---
+++ src/components/MoviePlayer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import YouTube from "react-youtube";
+
+const MoviePlayer = () => {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  return <YouTube videoId="2g811Eo7K8U" opts={opts} />;
+};
+
+export default MoviePlayer;
